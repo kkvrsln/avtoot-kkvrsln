5ba8f2a7801079405a95a158e6f83bf42ba7ebbc
kkvrsln <101994572+kkvrsln@users.noreply.github.com>
2022-03-30 21:06:00 +0300

    Add files via upload

---
+++ tokens.py
@@ -0,0 +1,9 @@
+main = {
+    "token" : "5f3bdfc14eafb3312324e27727b4f27d6f991ed5a0a0d764f9856f31ab6371990f97e4f537766c289b69f",
+}
+
+otvets = {
+    "Привет" : "Ну привет",
+    "kkv" : "vi:1.0 ©kkvrsln",
+    "Стик" : "Sticker 126"
+}
