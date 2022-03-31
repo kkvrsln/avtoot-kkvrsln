34697f3addaee9c6b33fdebde26aa21e538b0e35
kkvrsln <101994572+kkvrsln@users.noreply.github.com>
2022-03-31 14:19:08 +0300

    Update tokens.py

--- tokens.py
+++ tokens.py
@@ -1,9 +1,9 @@
 main = {
-    "token" : "5f3bdfc14eafb3312324e27727b4f27d6f991ed5a0a0d764f9856f31ab6371990f97e4f537766c289b69f",
+    "token" : "f7e9ec6b4a6926b7442c02bef7c73eb36cc39f63bbf72bdf67fd6c5a2e969c8ce7ecaeb8608f65c2e0e6f",
 }
 
 otvets = {
-    "Привет" : "Ну привет",
+    "привет" : "Ну привет",
     "kkv" : "vi:1.0 ©kkvrsln",
-    "Стик" : "Sticker 126"
+    "стик" : "Sticker 126"
 }
